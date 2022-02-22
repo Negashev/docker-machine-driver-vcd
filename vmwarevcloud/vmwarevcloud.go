0aa41a840652d06129c1689d9b2451141ac348cf
Negash <i@negash.ru>
2022-02-22 17:33:00 +0000

    fix resolv

--- vmwarevcloud/vmwarevcloud.go
+++ vmwarevcloud/vmwarevcloud.go
@@ -474,7 +474,7 @@ func (d *Driver) Create() error {
 	}
 
 	// fix resolv
-	GuestCustomizationSection.CustomizationScript += "\nsed -i_bak \"s/\\(nameserver\\) .*/\\1 127.0.0.53\nnameserver 1.1.1.1/\" /etc/resolv.conf\n\n"
+	GuestCustomizationSection.CustomizationScript += "\nsed -i_bak \"s/\\(nameserver\\) .*/\\1 127.0.0.53/\\nnameserver 1.1.1.1/\" /etc/resolv.conf\n\n"
 
 	GuestCustomizationSection.CustomizationScript += d.UserData
 
