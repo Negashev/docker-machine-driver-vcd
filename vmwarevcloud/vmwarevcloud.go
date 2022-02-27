fa3b3a21a2aa8442204ad1cd05bc313223c461b2
Negash <i@negash.ru>
2022-02-27 17:20:11 +0000

    Add set hostname in CustomizationScript

--- vmwarevcloud/vmwarevcloud.go
+++ vmwarevcloud/vmwarevcloud.go
@@ -495,6 +495,8 @@ func (d *Driver) Create() error {
 	GuestCustomizationSection.AdminPasswordEnabled = takeBoolPointer(false)
 
 	GuestCustomizationSection.CustomizationScript = d.InitData + "\n"
+	
+	GuestCustomizationSection.CustomizationScript += "hostname " + d.MachineName + "\necho " + d.MachineName + " | sudo tee /etc/hostname\n\n"
 	// add user
 	GuestCustomizationSection.CustomizationScript += "\nuseradd -m -d /home/" + d.SSHUser + " -s /bin/bash " + d.SSHUser + "\nmkdir -p /home/" + d.SSHUser + "/.ssh\nchown -R " + d.SSHUser + ":" + d.SSHUser + " /home/" + d.SSHUser + "/.ssh\nchmod 700 /home/" + d.SSHUser + "/.ssh\nchmod 600 /home/" + d.SSHUser + "/.ssh/authorized_keys\nusermod -a -G sudo " + d.SSHUser + "\necho \"" + strings.TrimSpace(key) + "\" > /home/" + d.SSHUser + "/.ssh/authorized_keys\npasswd -d " + d.SSHUser + "\nswapoff -a\nrm -rf /swap.img\n"
 
