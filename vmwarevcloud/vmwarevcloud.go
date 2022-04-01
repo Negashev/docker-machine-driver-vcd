0729960a7187a472de31a45ce030aecaf1f21d6f
Negash <i@negash.ru>
2022-04-01 16:27:24 +0500

    Add open-iscsi

--- vmwarevcloud/vmwarevcloud.go
+++ vmwarevcloud/vmwarevcloud.go
@@ -505,7 +505,7 @@ func (d *Driver) Create() error {
 	if strings.HasPrefix(d.CatalogItem, "ubuntu18") {
 		GuestCustomizationSection.CustomizationScript += "\nrm -rf /swapfile\n"
 		GuestCustomizationSection.CustomizationScript += "\napt update\n"
-		GuestCustomizationSection.CustomizationScript += "\nDEBIAN_FRONTEND=noninteractive apt -y install cloud-guest-utils curl\ngrowpart /dev/sda 2\nresize2fs /dev/sda2\n"
+		GuestCustomizationSection.CustomizationScript += "\nDEBIAN_FRONTEND=noninteractive apt -y install cloud-guest-utils curl open-iscsi\ngrowpart /dev/sda 2\nresize2fs /dev/sda2\n"
 		GuestCustomizationSection.CustomizationScript += "\nuserdel -r ubuntu || echo true\n"
 	}
 
